7c9c7ae246fcc037a7f322716688e7d7eb10d1a1
saigovardhan <veggalam.saigovardhan@gmail.com>
2021-12-19 23:28:17 +0530

    added cart visability state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import CartProvider from './store/CartProvider';
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import CartProvider from "./store/CartProvider";
+import { useState } from "react";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState();
+  function showCartHandler() {
+    setCartIsShown(true);
+  }
+  function hideCartHandler() {
+    setCartIsShown(false);
+  }
   return (
     <CartProvider>
-      <Header />
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
